c57cad65fb3a5c7d3ef40f1c099fdf216bb53e96
Rob Graham <r.ggraham@hotmail.com>
2021-08-08 12:19:08 +0100

    Bit of cleanup

--- sakura_story/src/main.cpp
+++ sakura_story/src/main.cpp
@@ -96,49 +96,32 @@ void gameMain() {
 	PROFILE_END_SESSION();
 }
 
-std::vector<char> g_buffer(1024 * 1024);
-
-void readData(asio::ip::tcp::socket& socket) {
-	socket.async_read_some(asio::buffer(g_buffer.data(), g_buffer.size()),
-		[&socket](std::error_code errorCode, std::size_t length) {
-			if (!errorCode) {
-				Log::info("readData - Read {} bytes", length);
-				for (int i = 0; i < length; ++i) {
-					std::cout << g_buffer[i];
-				}
-
-				readData(socket);
-			}
-			else {
-				Log::error("readData - Error reading from socket: {}", errorCode.message());
-			}
-		}
-	);
-}
-
 enum Message_Types : u32 {
 	Server_GetStatus,
 	Server_GetPing,
 	Server_Register,
 	Server_Unregister,
-	
+
 	Client_Accepted,
 	Client_AssignID,
-	
+
 	MessageAllClients,
 };
 
 class Game_Server : public server::net::Server {
 public:
-	Game_Server(u32 port) : Server(port) {}
+	Game_Server(u16 port) : Server(port) {}
 
 	bool onClientConnect(std::shared_ptr<common::net::Connection> client) override {
 		common::net::Message message;
 		message.header.id = Message_Types::Client_Accepted;
-
 		client->send(message);
-		
+
 		return true;
+	}
+
+	void onClientDisconnect(std::shared_ptr<common::net::Connection> client) override {
+		Log::info("[Server] Client disconnected, ID: {}", client->getId());
 	}
 
 	void onMessage(std::shared_ptr<common::net::Connection> client, common::net::Message& message) override {
@@ -147,7 +130,7 @@ public:
 				Log::info("[Server] Received ping from client: {}", client->getId());
 				client->send(message);
 			}
-			break;
+											  break;
 
 			case Message_Types::Server_Register: {
 				Log::info("[Server] Registering client: {}", client->getId());
@@ -156,7 +139,7 @@ public:
 				assignMessage << client->getId();
 				client->send(assignMessage);
 			}
-			break;
+											   break;
 
 			case Message_Types::MessageAllClients: {
 				Log::info("[Server] Message all from client: {}", client->getId());
@@ -195,89 +178,105 @@ int main(int argc, char* argv[]) {
 		type = ctype;
 	}
 
+	std::thread serverThread;
 	if (type == "--server") {
-		Game_Server server(60000);
-		server.start();
+		serverThread = std::thread(
+			[]() {
+				Game_Server server(60000);
+				server.start();
 
-		while (1) {
-			server.processMessages();
-		}
-	}
-	else {
-		Game_Client client1;
-		client1.connect("127.0.0.1", 60000);
-
-		bool key[3] = { false, false, false };
-		bool oldKey[3] = { false, false, false };
-
-		bool running = true;
-		while (running) {
-			if (GetForegroundWindow() == GetConsoleWindow()) {
-				key[0] = GetAsyncKeyState('1') & 0x8000;
-				key[1] = GetAsyncKeyState('2') & 0x8000;
-				key[2] = GetAsyncKeyState('3') & 0x8000;
-			}
-
-			if (key[0] && !oldKey[0]) {
-				client1.pingServer();
-			}
-			if (key[1] && !oldKey[1]) {
-				client1.messageAll();
-			}
-			if (key[2] && !oldKey[2]) {
-				running = false;
-			}
-
-			for (int i = 0; i < 3; ++i) {
-				oldKey[i] = key[i];
-			}
-
-			if (client1.isConnected()) {
-				if (!client1.incoming().isEmpty()) {
-					auto message = client1.incoming().popFront().message;
-
-					switch (message.header.id) {
-						case Message_Types::Server_GetPing: {
-							std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
-							std::chrono::system_clock::time_point then;
-							message >> then;
-
-							Log::info("[{}] Ping: {}", client1.getId(), std::chrono::duration<double>(now - then).count());
-						}
-						break;
-
-						case Message_Types::MessageAllClients: {
-							u32 clientId;
-							message >> clientId;
-
-							Log::info("[{}] Received message from client: {}", client1.getId(), clientId);
-						}
-						break;
-
-						case Message_Types::Client_Accepted: {
-							Log::info("[Unknown] Server accepted request");
-							common::net::Message message;
-							message.header.id = Message_Types::Server_Register;
-							client1.send(message);
-						}
-						break;
-
-						case Message_Types::Client_AssignID: {
-							u32 clientId;
-							message >> clientId;
-							client1.setId(clientId);
-							Log::info("[{}] Client registered with ID: {} ", client1.getId(), client1.getId());
-						}
-						break;
-					}
+				while (1) {
+					server.processMessages();
 				}
 			}
-			else {
-				Log::info("[{}] Server down!", client1.getId());
-				running = false;
+		);
+	}
+
+	std::thread clientThread = std::thread(
+		[]() {
+			Game_Client client1;
+			client1.connect("127.0.0.1", 60000);
+
+			bool key[3] = { false, false, false };
+			bool oldKey[3] = { false, false, false };
+
+			bool running = true;
+			while (running) {
+				if (GetForegroundWindow() == GetConsoleWindow()) {
+					key[0] = GetAsyncKeyState('1') & 0x8000;
+					key[1] = GetAsyncKeyState('2') & 0x8000;
+					key[2] = GetAsyncKeyState('3') & 0x8000;
+				}
+
+				if (key[0] && !oldKey[0]) {
+					client1.pingServer();
+				}
+				if (key[1] && !oldKey[1]) {
+					client1.messageAll();
+				}
+				if (key[2] && !oldKey[2]) {
+					running = false;
+				}
+
+				for (int i = 0; i < 3; ++i) {
+					oldKey[i] = key[i];
+				}
+
+				if (client1.isConnected()) {
+					if (!client1.incoming().isEmpty()) {
+						auto message = client1.incoming().popFront().message;
+
+						switch (message.header.id) {
+							case Message_Types::Server_GetPing: {
+								std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+								std::chrono::system_clock::time_point then;
+								message >> then;
+
+								Log::info("[{}] Ping: {}", client1.getId(), std::chrono::duration<double>(now - then).count());
+							}
+															  break;
+
+							case Message_Types::MessageAllClients: {
+								u32 clientId;
+								message >> clientId;
+
+								Log::info("[{}] Received message from client: {}", client1.getId(), clientId);
+							}
+																 break;
+
+							case Message_Types::Client_Accepted: {
+								Log::info("[Unknown] Server accepted request");
+								common::net::Message msgRegister;
+								msgRegister.header.id = Message_Types::Server_Register;
+								client1.send(msgRegister);
+							}
+															   break;
+
+							case Message_Types::Client_AssignID: {
+								u32 clientId;
+								message >> clientId;
+								client1.setId(clientId);
+								Log::info("[{}] Client registered with ID: {} ", client1.getId(), client1.getId());
+							}
+															   break;
+						}
+					}
+				}
+				else {
+					Log::info("[{}] Server down!", client1.getId());
+					running = false;
+				}
 			}
 		}
+	);
+
+	if (serverThread.joinable()) {
+		serverThread.join();
 	}
+	if (clientThread.joinable()) {
+		clientThread.join();
+	}
+
 
 	return (0);
 }
